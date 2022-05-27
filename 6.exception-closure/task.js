1186b4bab67837ea06fd5dc7a0a62493797460ee
Юлия Сенина <JuLKa9449@yandex.ru>
2022-05-27 14:23:28 +0300

    first commit variables

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,48 @@
+function parseCount(numberOfGoods) {
+  const conversionNumber = Number.parseInt(numberOfGoods, 10);
+    
+  if (Number.isNaN(conversionNumber)) {
+    throw new Error("Невалидное значение");
+  };
+
+  return conversionNumber;
+}
+  
+function validateCount(numberOfGoods) {
+  try {
+    return parseCount(numberOfGoods);
+  } catch(err) {
+    return err;
+  }
+};
+  
+  /*--2 задача--*/
+  
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+      
+    if (((a + b) < c) || ((a + c) < b) || ((b + c) < a)) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    };
+  };
+    
+  getPerimeter() {
+    return this.a + this.b + this.c;
+  };
+  
+  getArea() {
+    const p = getPerimeter() / 2;
+    return (Math.sqrt(p(p - this.a)(p - this.b)(p - this.c))).toFixed(3);
+  };
+};
+
+function getTriangle(a, b, c) {
+  try {
+    return new Object(Triangle);
+  } catch(err) {
+    
+  }
+}
