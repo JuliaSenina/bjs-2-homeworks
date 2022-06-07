02db5193b1e283c7b84a3fc894eb8330a1f4c2fd
Юлия Сенина <JuLKa9449@yandex.ru>
2022-06-07 14:17:56 +0300

    second commit variables

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,10 +1,42 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
-}
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let objectInCache = cache.find((item) => item.hash === hash);
+      if (!objectInCache) {
+        console.log('Из кэша: ' + cache[hash]);
+        return 'Из кэша: ' + cache[hash];  
+              
+      };
+        let result = func(...args);
+        cache.push({hash}); 
+    
+      if (cache.length > 5) {
+        cache.shift(hash);
+      }; 
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;     
+  };    
+    
+  return wrapper;
+};
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timeout;
+  func();
+  let flag = true;
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      if (!flag) {
+        func.apply(this, args);
+        console.timeEnd("time");
+      };
+    }, ms);
+  };   
 }
 
 function debounceDecorator2(func) {
